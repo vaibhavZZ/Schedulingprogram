e16183978a300d88014e00adb7880d846a527761
vaibhavZZ <33392515+vaibhavZZ@users.noreply.github.com>
2018-04-07 21:29:12 +0530

    Update schdlprjct.c

--- schdlprjct.c
+++ schdlprjct.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 struct process
 {
+int process_id;
 int arrival_time;
 int burst_time;
 int priority;
@@ -10,13 +11,14 @@ int turn_around_time;
 };
 void main()
 {
-int i,n,temp,j,temp1,temp2;
+int i,n,x,y,temp,j,temp1,temp2,temp3;
 printf("Enter total number of procescs");
 scanf("%d",&n);
 struct process p[n];
 for(i=0;i<n;i++)
 {
-printf("PROCESS ID := %d\n",i);
+printf("PROCESS ID := ");
+scanf("%d",&p[i].process_id);
 printf("ARRIVAL TIME := ");
 scanf("%d",&p[i].arrival_time);
 printf("BURST TIME := ");
@@ -40,14 +42,34 @@ p[i+1].burst_time=temp1;
 temp2=p[i].priority;
 p[i].priority=p[i+1].priority;
 p[i+1].priority=temp2;
+temp3=p[i].process_id;
+p[i].process_id=p[i+1].process_id;
+p[i+1].process_id=temp3;
 }
 }
 }
 for(i=0;i<n;i++)
 {
-p[i].completion_time=p[i].burst_time;
+if(i==0)
+{
+p[i].completion_time=p[i].burst_time+p[i].arrival_time;
 p[i].turn_around_time=p[i].completion_time-p[i].arrival_time;
 p[i].waiting_time=p[i].turn_around_time-p[i].burst_time;
+}
+else if(p[i].arrival_time>p[i-1].completion_time)
+{
+y=p[i].burst_time;
+p[i].completion_time=p[i].arrival_time+y;
+p[i].turn_around_time=p[i].completion_time-p[i].arrival_time;
+p[i].waiting_time=p[i].turn_around_time-p[i].burst_time;
+}
+else
+{
+x=p[i].burst_time;
+p[i].completion_time=p[i-1].completion_time+x;
+p[i].turn_around_time=p[i].completion_time-p[i].arrival_time;
+p[i].waiting_time=p[i].turn_around_time-p[i].burst_time;
+}
 }
 printf("*****PROCESS CHART*****\n\n");
 printf("PROCESS ID\t");
@@ -59,7 +81,7 @@ printf(" TURN AROUND TIME\t");
 printf("WAITING TIME\t\n");
 for(i=0;i<n;i++)
 {
-printf("%d\t\t",i);
+printf("%d\t\t",p[i].process_id);
 printf("%d\t\t",p[i].arrival_time);
 printf("%d\t\t",p[i].burst_time);
 printf("%d\t\t",p[i].priority);
